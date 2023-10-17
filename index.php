b8274c297835a194f78af07ebba62dbcb2448066
Fillipe <fillipe@every.is>
2023-10-17 11:31:21 -0400

    update title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Brothers Todo App</title>
+    <title>Brothers Todo App - UPDATED VERSION</title>
 
     <!-- css -->
     <link rel="stylesheet" href="style.css">

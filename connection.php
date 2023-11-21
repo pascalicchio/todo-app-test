e7cc863209121c62d5c4b963c5bd3b4fb6137106
Fillipe <fillipe@every.is>
2023-11-21 09:39:41 -0500

    fix connection

--- connection.php
+++ connection.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * main-2023-11-21-9oh5k9
+ * DB_HOST=aws.connect.psdb.cloud
+ * DB_USERNAME=f0nk9o1ntknjnou4yy0s
+ * DB_PASSWORD=pscale_pw_gnHjN5PPW82PTACMePVmhCSBpsQ6nZ16V3dNtkYOlF1
+ * DB_NAME=test
+ */
 require 'vendor/autoload.php'; // Load Composer autoloader
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -7,7 +14,7 @@ $dotenv->load();
 // Connect to PlanetScale using credentials stored in environment variables
 $conn = mysqli_init();
 $conn->ssl_set(NULL, NULL, "/etc/ssl/cert.pem", NULL, NULL);
-$conn->real_connect('us-east.connect.psdb.cloud', '5xxc7i38svk7qo601u7a', 'pscale_pw_bdSJVAUqS7dJOmf6WHb7ktAfaaXhNkwDA43Kn3oM0Vx', 'test');
+$conn->real_connect('aws.connect.psdb.cloud', 'f0nk9o1ntknjnou4yy0s', 'pscale_pw_gnHjN5PPW82PTACMePVmhCSBpsQ6nZ16V3dNtkYOlF1', 'test');
 
 // Check connection
 if ($conn->connect_errno) {

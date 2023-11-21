a9f5bbd216a8f8c9d806617510cbe545e0f59637
Fillipe <fillipe@every.is>
2023-11-21 09:49:07 -0500

    reset branch

--- connection.php
+++ connection.php
@@ -1,13 +1,23 @@
 <?php
-// connecting to the database
-$localserver = '127.0.0.1';
-$username = 'root';
-$password = '';
-$database = 'todo';
 
-$conn = new mysqli($localserver, $username, $password, $database);
+/**
+ * main-2023-11-21-9oh5k9
+ * DB_HOST=aws.connect.psdb.cloud
+ * DB_USERNAME=f0nk9o1ntknjnou4yy0s
+ * DB_PASSWORD=pscale_pw_gnHjN5PPW82PTACMePVmhCSBpsQ6nZ16V3dNtkYOlF1
+ * DB_NAME=test
+ */
+require 'vendor/autoload.php'; // Load Composer autoloader
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Connect to PlanetScale using credentials stored in environment variables
+$conn = mysqli_init();
+$conn->ssl_set(NULL, NULL, "/etc/ssl/cert.pem", NULL, NULL);
+$conn->real_connect('aws.connect.psdb.cloud', 'f0nk9o1ntknjnou4yy0s', 'pscale_pw_gnHjN5PPW82PTACMePVmhCSBpsQ6nZ16V3dNtkYOlF1', 'test');
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
+    exit();
 }
